f992f9de0bc44a55fd4b566a16436cace6a74b5d
tuongnhatnguyenvan <tuongnhatnv@gamail.com>
2024-06-18 15:49:34 +0700

    example for Session

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,8 @@
-import express from 'express';
+import express, { request, response } from 'express';
 import routers from './routes/index.mjs';
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
+import { mockUsers } from './utils/constants.mjs';
 
 const app = express();
 
@@ -11,9 +12,9 @@ app.use(session({
   secret: "nhat dev",
   resave: false,
   saveUninitialized: false,
-  cookie: { 
+  cookie: {
     maxAge: 60000 * 60,
-   }
+  }
 }));
 app.use(routers);
 const PORT = process.env.PORT || 3000;
@@ -33,9 +34,50 @@ app.use(loggingMiddleware, (request, response, next) => {
 });
 
 app.get("/", (request, response) => {
-  console.log("Session: ",request.session);
-  console.log("Session ID: ",request.session.id);
+  console.log("Session: ", request.session);
+  console.log("Session ID: ", request.session.id);
   request.session.visited = true;
   response.cookie("hello", "word", { maxAge: 30000, signed: true });
   response.status(201).send({ msg: 'Hello' });
 });
+
+app.post("/api/auth", (request, response) => {
+  const { body: { username, password } } = request;
+  const findUser = mockUsers.find((user) => user.username === username);
+  if (!findUser || findUser.password !== password) {
+    return response.status(401).send({ msg: "Invalid username or password" });
+  } else {
+    request.session.user = findUser;
+    return response.status(200).send({ msg: "Login success", user: findUser });
+  }
+});
+
+app.get("/api/auth/status", (request, response) => {
+  request.sessionStore.get(request.sessionID, (err, session) => {
+    console.log("Session: ", session);
+  });
+  return request.session.user ? response.status(200).send(request.session.user) : response.status(401).send({ msg: "Not Authenticated" });
+})
+
+app.post("/api/cart", (request, response) => {
+  if (!request.session.user) {
+    return response.status(401).send({ msg: "Not Authenticated" });
+  }
+  const item = request.body;
+  console.log("Item: ", item);
+  const { cart } = request.session;
+  if (cart) {
+    cart.push(item);
+  }
+  else {
+    request.session.cart = [item];
+  }
+  return response.status(200).send({ msg: "Added to cart", cart });
+})
+
+app.get("/api/cart", (request, response) => {
+  if (!request.session.user) {
+    return response.status(401).send({ msg: "Not Authenticated" });
+  }
+  return response.send(request.session.cart ?? []);
+})
--- src/utils/constants.mjs
+++ src/utils/constants.mjs
@@ -1,52 +1,62 @@
 export const mockUsers = [
-    {
-        id: 1,
-        username: "John",
-        displayName: "Doe",
-    },
-    {
-        id: 2,
-        username: "Jane",
-        displayName: "Doe",
-    },
-    {
-        id: 3,
-        username: "Alice",
-        displayName: "Smith",
-    },
-    {
-        id: 4,
-        username: "Bob",
-        displayName: "Johnson",
-    },
-    {
-        id: 5,
-        username: "Charlie",
-        displayName: "Williams",
-    },
-    {
-        id: 6,
-        username: "David",
-        displayName: "Brown",
-    },
-    {
-        id: 7,
-        username: "Eve",
-        displayName: "Davis",
-    },
-    {
-        id: 8,
-        username: "Frank",
-        displayName: "Miller",
-    },
-    {
-        id: 9,
-        username: "Grace",
-        displayName: "Wilson",
-    },
-    {
-        id: 10,
-        username: "Henry",
-        displayName: "Moore",
-    },
+  {
+    id: 1,
+    username: "John",
+    displayName: "Doe",
+    password: "password1",
+  },
+  {
+    id: 2,
+    username: "Jane",
+    displayName: "Doe",
+    password: "password2",
+  },
+  {
+    id: 3,
+    username: "Alice",
+    displayName: "Smith",
+    password: "password3",
+  },
+  {
+    id: 4,
+    username: "Bob",
+    displayName: "Johnson",
+    password: "password4",
+  },
+  {
+    id: 5,
+    username: "Charlie",
+    displayName: "Williams",
+    password: "password5",
+  },
+  {
+    id: 6,
+    username: "David",
+    displayName: "Brown",
+    password: "password6",
+  },
+  {
+    id: 7,
+    username: "Eve",
+    displayName: "Davis",
+    password: "password7",
+  },
+  {
+    id: 8,
+    username: "Frank",
+    displayName: "Miller",
+    password: "password8",
+  },
+  {
+    id: 9,
+    username: "Grace",
+    displayName: "Wilson",
+    password: "password9",
+  },
+  {
+    id: 10,
+    username: "Henry",
+    displayName: "Moore",
+    password: "password10",
+  },
 ];
